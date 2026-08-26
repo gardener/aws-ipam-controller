f5dbe679fffa0a324271b53588e888747f7d58a5
Marc Vornetran <marc.vornetran@sap.com>
2026-08-26 13:55:50 +0200

    Update copyright text
    Signed-off-by: Marc Vornetran <marc.vornetran@sap.com>

--- pkg/ipam/cidrset/metrics.go
+++ pkg/ipam/cidrset/metrics.go
@@ -16,7 +16,7 @@
 // This file was copied from the kubernetes/cloud-provider-gcp project
 // https://github.com/kubernetes/cloud-provider-gcp/blob/master/pkg/controller/nodeipam/ipam/cidrset/metrics.go
 //
-// Modifications Copyright 2024 SAP SE or an SAP affiliate company and Gardener contributors
+// Modifications Copyright Copyright Contributors to the Gardener project
 
 package cidrset
 
